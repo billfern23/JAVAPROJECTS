0d9086f31ca23e85864c0ff1b550f5595d87e18f
Billy Fernandes <56287260+billfern23@users.noreply.github.com>
2022-02-09 11:12:25 -0500

    Add files via upload

---
+++ Objectequality/src/ObjectEquality.java
@@ -0,0 +1,18 @@
+public class ObjectEquality {
+	
+	public static void main(String[] args) {
+		String x = "1";
+		String y = new String("1");// check for String y = "1";
+
+		if (x == y)
+			System.out.println("Equal");
+		else
+			System.out.println("Not Equal");
+		
+		if(x.equals(y)) {
+			
+			System.out.println("They are good");
+		}
+	}
+
+}
