3e98dde75321897132423b6fec19cb1227e5549f
Billy Fernandes <56287260+billfern23@users.noreply.github.com>
2022-02-09 11:20:11 -0500

    Delete src directory

--- src/shapes/Area.java
+++
@@ -1,8 +0,0 @@
-package shapes;
-//calculateArea doesnt recieve an arguement because i tried double value, but because of rectangle would need two values, it dint work
-//So i did double... value but that would mean dealing with an array and doesnt make sense when its a cirle, so i used the fact that lambdas
-//have access to local values to create my object.
-@FunctionalInterface
-public interface Area {
-	double calculateArea();
-}
--- src/shapes/Shape.java
+++
@@ -1,7 +0,0 @@
-package shapes;
-// Shape class, this the interface or bases for all the other shapes to inherit from except parallelogram.
-@FunctionalInterface
-public interface Shape {
-	 double calculatePerimeter();
-	 
-}
